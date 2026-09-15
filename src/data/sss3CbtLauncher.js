2e69cfaf2d618de49411851bbc0a4e854a84d6ca
GBENGA OLUFISAYO <77745840+Gbenga512@users.noreply.github.com>
2026-09-15 05:49:42 +0100

    Add JAMB to examination centre

--- src/data/sss3CbtLauncher.js
+++ src/data/sss3CbtLauncher.js
@@ -1,2 +1,2 @@
-export const SSS3_CBT_LAUNCHER={title:'SSS 3 Examination Centre',subtitle:'Prepare for WAEC and NECO Mathematics',exams:[{id:'WAEC',label:'WAEC Mathematics',duration:'150 minutes',questions:50},{id:'NECO',label:'NECO Mathematics',duration:'150 minutes',questions:50}]};
+export const SSS3_CBT_LAUNCHER={title:'SSS 3 Examination Centre',subtitle:'Prepare with dedicated WAEC, NECO and JAMB-style Mathematics practice.',exams:[{id:'WAEC',label:'WAEC Mathematics',duration:'150 minutes',questions:50},{id:'NECO',label:'NECO Mathematics',duration:'150 minutes',questions:50},{id:'JAMB',label:'JAMB UTME Mathematics Practice',duration:'90 minutes',questions:50}]};
 export const getSSS3CbtExam=id=>SSS3_CBT_LAUNCHER.exams.find(x=>x.id===id)||SSS3_CBT_LAUNCHER.exams[0];
